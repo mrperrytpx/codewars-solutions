688825e6b0ab6801e29069b6cafb7345bba9f959
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-04 18:50:28 +0100

    password validation kata

--- Password_Validation.js
+++ Password_Validation.js
@@ -1,1 +1,13 @@
 // https://www.codewars.com/kata/52e1476c8147a7547a000811/javascript
+// ORIGINAL SOLUTION
+function validate(password) {
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/.test(password);
+}
+
+// EXAMPLE INPUT
+let pw = "Password123"; // expected FALSE
+console.log(validate(pw));
+
+// REFACTORED SOLUTION
+const val = password => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/.test(password);
+console.log(val(pw));
