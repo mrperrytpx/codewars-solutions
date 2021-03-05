0377310af9518c6264180e896dfc415083c5c079
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-05 22:57:14 +0100

    next kata

---
+++ Life_Path_Numbers.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5a1a76c8a7ad6aa26a0007a0
+
+// ORIGINAL SOLUTION
+function lifePathNumber(dateOfBirth) {
+    // Your solution...
+    
+    return lifenum;
+}
+
+// EXMAPLE INPUT 
+const input = "1879-03-14";
+
+console.log(lifePathNumbeR(input));
