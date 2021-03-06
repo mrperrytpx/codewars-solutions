164cb5936460bdcbae9b6d126ece991d14d382e5
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-06 19:04:43 +0100

    reverse kata

---
+++ Reverser.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/58069e4cf3c13ef3a6000168/train/javascript
+
+// EXAMPLE INPUT 
+const input = 1234; // result = 4321
+
+// ORIGINAL SOLUTION
+function reverse(n){
+    let rev = 0;
+    while (n > 0) {
+        rev = (rev * 10) + (n % 10);
+        n = Math.floor(n / 10);
+    }
+    return rev;
+}
+
+console.log(reverse(input));
