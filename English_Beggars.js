e9ef1bbd4f465571c0de146279d3fce337fc957f
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-06 19:17:15 +0100

    next

---
+++ English_Beggars.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/59590976838112bfea0000fa
+
+// EXAMPLE INPUT
+let inputArray = [1,2,3,4,5];
+let inputNumber = 1;
+// result = [15]
+
+//ORIGINAL SOLUTION
+function beggars(values, n){
+    //your code here
+}
+
+console.log(beggars, array, input)
