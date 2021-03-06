596289f4facccc5d955083907c448feb537ae246
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-06 21:35:23 +0100

    NEZNAM NEZNAM NEZNAM NEZNAM NEZNAM NEZNAM NEZNAM NEZNAM NEZNAM NEZNAM NEZNAM AAAAAAAAAAAAAAAAAAAAAAAAA

--- English_Beggars.js
+++ English_Beggars.js
@@ -1,13 +1,18 @@
 // https://www.codewars.com/kata/59590976838112bfea0000fa
 
 // EXAMPLE INPUT
-let inputArray = [1,2,3,4,5];
-let inputNumber = 1;
-// result = [15]
+let inputArray = [1, 2, 3, 4, 5]; // => [1, 3, 5, 2, 4] => [9, 6]
+let inputNumber = 3;
 
 //ORIGINAL SOLUTION
-function beggars(values, n){
-    //your code here
+function beggars(values, n) {
+    let array = [];
+    for (let i = 0; i < values.length; i++) {
+        if (i % n === 0) {
+            array.push(values[i]);
+        }
+    }
+    return array;
 }
 
-console.log(beggars, array, input)
+console.log(beggars(inputArray, inputNumber));
