2ab185916519be84b00f51fb01d3e70de146b900
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-10 20:18:22 +0100

    dis dick

---
+++ Happy_Number.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/59d53c3039c23b404200007e/train/javascript
+// EXAMPLE INPUT
+const input = 10 // result => [1, 7, 10];
+
+//ORIGINAL SOLUTION 
+function happyNumbers(x){
+    // Put your answer here
+    return 0;
+}
