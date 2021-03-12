b5dc8ab2c70524a220179caa1401dcb60e7df910
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-12 17:12:00 +0100

    Testing 123 kata

---
+++ Testing_1_2_3.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+// EXAMPLE INPUT
+const input = ["a", "b", "c"]; // result -> ["1: a", "2: b", "3: c"]
+
+//SOLUTION
+let number = function(array){
+    return array = array.map((entry, index) => `${index + 1}: ${entry}`);
+}
+
+console.log(number(input));
