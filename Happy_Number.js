56061c2ee1eadf3f25ad1bdc3150c365f8bce2c6
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-03-12 15:20:10 +0100

    Happy numbers kata

--- Happy_Number.js
+++ Happy_Number.js
@@ -1,9 +1,50 @@
 // https://www.codewars.com/kata/59d53c3039c23b404200007e/train/javascript
 // EXAMPLE INPUT
-const input = 10 // result => [1, 7, 10];
+const input = 13 // result => [1, 7, 10];
 
-//ORIGINAL SOLUTION 
+//ORIGINAL SOLUTION
+/* function digits(x) {
+    return x.toString().split("").map(num => parseInt(num) ** 2).reduce((acc, curr) => acc + curr);
+}
+
+function square(x, arr = []) {
+    if (x === 1 && arr.length > 0) {
+        return arr[0];
+    } else {
+        if (arr.includes(x)) {
+            return;
+        }
+        arr.push(x);
+        x = digits(x);
+        return square(x, arr);
+    }
+}
+
 function happyNumbers(x){
-    // Put your answer here
-    return 0;
+    let resultArray = [];
+    for (let i = 1; i <= x; i++) {
+        resultArray.push(square(i));
+    }
+    resultArray = resultArray.filter(elem => elem > 0);
+    return resultArray;
+} */
+
+console.log(happyNumbers(input));
+
+//REFACTORED SOLUTION
+function square(x, arr = []) {
+    if (x === 1 && arr.length > 0) {
+        return arr[0];
+    } else {
+        if (arr.includes(x)) return;
+        arr.push(x);
+        x = x.toString().split("").map(num => parseInt(num) ** 2).reduce((acc, curr) => acc + curr);
+        return square(x, arr);
+    }
+}
+
+function happyNumbers(x){
+    let resultArray = [];
+    for (let i = 1; i <= x; i++) resultArray.push(square(i));
+    return resultArray.filter(elem => elem > 0);
 }
