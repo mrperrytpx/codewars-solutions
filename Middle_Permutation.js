4b2ecf319502a1326a166ea91d75a1802fe31858
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-04-07 20:12:44 +0200

    Middle permutation working kata

--- Middle_Permutation.js
+++ Middle_Permutation.js
@@ -1,9 +1,12 @@
 // https://www.codewars.com/kata/58ad317d1541651a740000c5/train/javascript
 
 // EXAMPLE INPUT
-const input = "abc" // solution => abc acb bac bca cab cba - math.floor arr length = bac
+const input = "wfcairbxklyqnhupvmes"
 
 // ORIGINAL SOLUTION - Why is it not ordering correctly I don't understand
+// DICTIONARY ORDER MEANS ALPHABETICALLY EACH TIME HAHAHAHHA NICE SO FUNNY :)
+// WORKS BUT INCEDIBLY SLOW FOR LONG STRINGS
+
 const permutation = string => {
     if (string.length < 2) return string;
     let resArray = [];
@@ -14,6 +17,7 @@ const permutation = string => {
         let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length);
 
         for (let perm of permutation(remainingChars)) {
+            console.log(char + perm);
             resArray.push(char + perm);
         }
     }
@@ -21,20 +25,20 @@ const permutation = string => {
 }
 
 const middlePermutation = (string) => {
-    console.log(string);
+    string = string.split("").sort().join("");
     let resultArr = permutation(string);
     let res = resultArr.length % 2 === 0 ? resultArr[resultArr.length / 2 - 1] : resultArr[resultArr.length / 2 - 1];
-    for (let i = 0; i < resultArr.length; i++) {
+/*     for (let i = 0; i < resultArr.length; i++) {
         console.log(`${i + 1} = ${resultArr[i]} - ${resultArr.length}`);
-    }
-    console.log(resultArr.indexOf("cxgdba"));
+    } */
+    // console.log(resultArr.indexOf("cxgdba"));
     return res;
 }
 
 console.log(middlePermutation(input));
 
-
-/* function middlePermutation(s) {
+// Most understandable solution I **YOINKED**
+function middlePermutation(s) {
     const sortedArr = s.split('').sort();
     if (sortedArr.length % 2 === 0) {
       return [...sortedArr.splice(sortedArr.length / 2 - 1, 1), ...sortedArr.reverse()].join('')
@@ -42,4 +46,4 @@ console.log(middlePermutation(input));
     else {
       return [...sortedArr.splice(sortedArr.length / 2 , 1), middlePermutation(sortedArr.join(''))].join('')
     }
-  } */
+  }
