5eae90eae82c69297acd967c8f3cb98f058f9f98
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-04-09 22:36:54 +0200

    Switcheroo kata

---
+++ Switcheroo.js
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/57f759bb664021a30300007d/train/javascript
+
+// EXAMPLE INPUT
+let input = "aabacbaa" // reulst -> 'bbabcabb'
+
+// FIRST SOLUTION
+function switcheroo(x) {
+    let result = [...x]
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] === "a") {
+            result[i] = "b";
+            continue;
+        }
+        if (result[i] === "b") {
+            result[i] = "a";
+            continue;
+        }
+    }
+    return result.join("");
+}
+
+console.log(switcheroo(input));
+
+// REFACTORED SOLUTION
+function switcheroo(x) {
+    let result = [...x].map(elem => {
+        if (elem === "a") return "b";
+        if (elem === "b") return "a";
+        if (elem === "c") return "c";
+    });
+    return result.join("");
+}
