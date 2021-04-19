cdcbf6c6c55f0e345ee55b2ce371f22a157d68e8
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-04-19 02:42:12 +0200

    All star #20 kata,raise an error can also be a new Erorr object smile :)

---
+++ 7 kyu/All_Star_Code_Challenge_#20.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5865a75da5f19147370000c7
+
+// EXAMPLE INPUT
+const input1 = [1, 2], input2 = [4, 5] // result -> [5, 7]
+
+// SOLUTION
+function addArrays(array1, array2) {
+    if (array1.length !== array2.length) throw new Error("Error");
+    let result = [];
+    for (let i = 0; i < array1.length; i++) {
+      result.push(array1[i] + array2[i]);
+    }
+    return result;
+}
+
+console.log(addArrays(input1, input2));
