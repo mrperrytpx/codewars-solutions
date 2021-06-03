74997f6579d7ce43417529f0d157e862ff7298e5
mrperrytpx <77341991+mrperrytpx@users.noreply.github.com>
2021-06-03 23:46:08 +0200

    Delete Isograms.js

--- 7 kyu/Isograms.js
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/54ba84be607a92aa900000f1
-
-// EXAMPLE INOUT
-const input = "Richard" // result -> true
-
-// SOLUTION
-const isIsogram = str => new Set(str.toLowerCase()).size === str.length;
-
-console.log(isIsogram(input));
