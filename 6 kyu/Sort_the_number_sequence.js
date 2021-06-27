fd848f43336a9620c4aac87e29f6af24f3c55f37
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-06-28 00:58:52 +0200

    MORE

--- 6 kyu/Sort_the_number_sequence.js
+++ 6 kyu/Sort_the_number_sequence.js
@@ -15,28 +15,24 @@ const chunkArray = (array) => {
     let indexes = getAllIndexes(array, 0);
     for (let i = 0; i < array.length; i++) {
         const first = indexes.shift();
-        resultArray.push(array.slice(i, first + 1));
+        const chunk = array.slice(i, first + 1).sort((a, b) => a - b).filter(num => num !== 0).concat(0);
+        resultArray.push(chunk);
         i = first;
     }
     return resultArray;
 }
 
-const reducer = array => array.reduce((acc, curr) => acc + curr, 0)
+const sum = array => array.reduce((acc, curr) => acc + curr, 0)
 
 const sortSequence = (sequence) => {
     const chunkedArray = chunkArray(sequence);
-
-    for (let i = 0; i < chunkedArray.length; i++) {
-        chunkedArray[i] = chunkedArray[i].sort((a, b) => a - b).concat(0);
-        chunkedArray[i].shift();
-    }
 
     let len = chunkedArray.length;
     let swapped;
     do {
         swapped = false;
         for (let i = 0; i < len - 1; i++) {
-            if (reducer(chunkedArray[i]) > reducer(chunkedArray[i + 1])) {
+            if (sum(chunkedArray[i]) > sum(chunkedArray[i + 1])) {
                 let tmp = chunkedArray[i];
                 chunkedArray[i] = chunkedArray[i + 1];
                 chunkedArray[i + 1] = tmp;
@@ -53,7 +49,7 @@ const sum = arr => arr.reduce((a, b) => a + b, 0);
 
 function sortSequence(sequence) {
     let res = [];
-    for (let i = 0; i < sequence.length; ++i) {
+    for (let i = 0; i < sequence.length; i++) {
         let temp = [];
         while (sequence[i] !== 0) temp.push(sequence[i++]);
         res.push({
