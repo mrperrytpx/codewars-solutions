d93e009c6b280309eb6968e2a6338bf058ba1184
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-06-28 00:48:08 +0200

    we do a little bit of refactoring :tf:

--- 6 kyu/Sort_the_number_sequence.js
+++ 6 kyu/Sort_the_number_sequence.js
@@ -4,11 +4,9 @@
 const input = [3, 2, 1, 0, 5, 6, 4, 0, 1, 5, 3, 0, 4, 2, 8, 0] // result [1,2,3,0,1,3,5,0,2,4,8,0,4,5,6,0]
 
 // FIRST SOLUTION
-function getAllIndexes(arr, val) {
-    var indexes = [], i;
-    for (i = 0; i < arr.length; i++)
-        if (arr[i] === val)
-            indexes.push(i);
+const getAllIndexes = (arr, val) => {
+    let indexes = [];
+    for (let i = 0; i < arr.length; i++) if (arr[i] === val) indexes.push(i);
     return indexes;
 }
 
@@ -17,35 +15,27 @@ const chunkArray = (array) => {
     let indexes = getAllIndexes(array, 0);
     for (let i = 0; i < array.length; i++) {
         const first = indexes.shift();
-        const chunk = array.slice(i, first + 1);
+        resultArray.push(array.slice(i, first + 1));
         i = first;
-        resultArray.push(chunk);
     }
     return resultArray;
 }
 
-const reducer = array => {
-    if (!array) {
-        return [0];
-    } else {
-        return array.reduce((acc, curr) => acc + curr, 0)
-    }
-};
+const reducer = array => array.reduce((acc, curr) => acc + curr, 0)
 
-function sortSequence(sequence) {
+const sortSequence = (sequence) => {
     const chunkedArray = chunkArray(sequence);
 
     for (let i = 0; i < chunkedArray.length; i++) {
-        chunkedArray[i] = chunkedArray[i].sort((a, b) => a - b);
+        chunkedArray[i] = chunkedArray[i].sort((a, b) => a - b).concat(0);
         chunkedArray[i].shift();
-        chunkedArray[i].push(0);
     }
 
     let len = chunkedArray.length;
     let swapped;
     do {
         swapped = false;
-        for (let i = 0; i < len; i++) {
+        for (let i = 0; i < len - 1; i++) {
             if (reducer(chunkedArray[i]) > reducer(chunkedArray[i + 1])) {
                 let tmp = chunkedArray[i];
                 chunkedArray[i] = chunkedArray[i + 1];
@@ -55,7 +45,7 @@ function sortSequence(sequence) {
         }
     } while (swapped);
 
-    return chunkedArray.filter(member => member !== undefined).reduce((acc, curr) => acc.concat(curr), []);
+    return chunkedArray.reduce((acc, curr) => acc.concat(curr), []);
 }
 
 // REFACTORED (STOLEN FROM SOMEONE);
