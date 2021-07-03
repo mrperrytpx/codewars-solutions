b47dabc1bd01524a1b1e523d99a6f17ba2a57a32
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-07-03 15:44:34 +0200

    forgot th elink

--- 6 kyu/Christmas_tree.js
+++ 6 kyu/Christmas_tree.js
@@ -1,3 +1,15 @@
+// https://www.codewars.com/kata/52755006cc238fcae70000ed
+
+// EXAMPLE INPUT
+const input = 5
+// result 
+//     *    
+//    ***   
+//   *****  
+//  ******* 
+// *********
+
+// SOLUTION
 function christmasTree(height) {
     let tree = [];
     for (let i = 1; i <= height; i++) {
