16e275b58db4880ca712f93c2d7bea746d945384
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-07-13 23:21:05 +0200

    Break camelCase kata

---
+++ 6 kyu/Break_camelCase.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5208f99aee097e6552000148
+
+// EXAMPLE INPUT
+const input = "camelCase" // result => camelCase
+
+// ORIGINAL SOLUTION
+function solution(string) {
+    let array = [...string];
+    for (let i = 0; i < array.length; i++) {
+        if (!/[a-z]/.test(array[i]) && /[A-Z]/.test(array[i])) {
+            array.splice(i, 0, " ");
+            i++;
+        }
+    }
+    return array.join("");
+}
+
+// REFACTORED SOLUTION
+const solution = (string) => string.replace(/([A-Z])/g, ' $1');
